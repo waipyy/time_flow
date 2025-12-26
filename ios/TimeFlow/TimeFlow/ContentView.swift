7c8ca6ed83495084a3cf804b1d8d973c8fee75a6
Pingyuan <pingyuanyue@gmail.com>
2025-12-25 23:42:47 -0800

    fix calendar view

--- ios/TimeFlow/TimeFlow/ContentView.swift
+++ ios/TimeFlow/TimeFlow/ContentView.swift
@@ -9,13 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Color.clear
-            Text("TimeFlow")
-                .font(.largeTitle)
-                .fontWeight(.medium)
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        CalendarView()
     }
 }
 
