26415a6232537a2bf40596cfa4387b70e58b2e1e
Pingyuan Yue <pingyuan@google.com>
2025-12-25 23:34:15 -0800

    xcode change

---
+++ ios/TimeFlow/TimeFlowTests/TimeFlowTests.swift
@@ -0,0 +1,17 @@
+//
+//  TimeFlowTests.swift
+//  TimeFlowTests
+//
+//  Created by Pingyuan Yue on 12/25/25.
+//
+
+import Testing
+@testable import TimeFlow
+
+struct TimeFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
