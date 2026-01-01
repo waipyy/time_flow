cb4a206a5eaa77b83e5ddb045f53e2dc93310244
Pingyuan Yue <pingyuanyue@gmail.com>
2026-01-01 22:41:05 +0000

    Try fixing this error: `Console Error: Error: [GoogleGenerativeAI Error]

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,5 +3,5 @@ import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
   plugins: [googleAI()],
-  model: 'googleai/gemini-1.5-flash-preview-0514',
+  model: 'googleai/gemini-1.5-flash',
 });
